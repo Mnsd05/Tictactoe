65e65701900ecbfcbfd59c8d375a146a5f5b86e0
Mnsd05 <thangngdc03bn@gmail.com>
2022-12-19 22:29:54 +0800

    update

--- first/Tictactoe.java
+++ first/Tictactoe.java
@@ -9,16 +9,16 @@ import java.awt.event.*;
 import java.awt.Image;
 public class Tictactoe implements ActionListener {
   JFrame frame1 = new JFrame(); 
-  JLayeredPane layeredPane = new JLayeredPane();     // to contain overlapping components
-  JPanel board = new JPanel();                       // to contain 9 cells
-  Label finalLabel = new Label();                    // Display the result
+  JLayeredPane layeredPane = new JLayeredPane();        // to contain overlapping components
+  JPanel board = new JPanel();                          // to contain 9 cells
+  Label finalLabel = new Label();
   MyButton[] buttonsList = new MyButton[9];
     Tictactoe ()  {            
       frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame1.setSize(new Dimension(500, 500));
       frame1.setResizable(false);
       frame1.setVisible(true);
-  
+    
       board.setBounds(0,0,480,460);
       board.setLayout(new GridLayout(3,3,10,10));
       
@@ -53,9 +53,9 @@ public class Tictactoe implements ActionListener {
   }
   @Override
 	public void actionPerformed(ActionEvent e) {
-    for (int i = 0; i < 9; ++i) {
-      	if(e.getSource()==buttonsList[i] && !buttonsList[i].ticked && MyButton.cantClick) {
-            MyButton.numClicked += 1;
+      for (int i = 0; i < 9; ++i) {
+      	  if(e.getSource()==buttonsList[i] && !buttonsList[i].ticked && !MyButton.cantClick) {
+                MyButton.numClicked += 1;
             if (MyButton.firstPlayer) {
                 ImageIcon tick = new ImageIcon(new ImageIcon("Tictactoe/first/tick.png").getImage().
                 getScaledInstance(50, 50, Image.SCALE_DEFAULT));
@@ -70,17 +70,17 @@ public class Tictactoe implements ActionListener {
             MyButton.firstPlayer ^= true;
             buttonsList[i].ticked = true;
             if (MyButton.checkBoard.checkWin()) {
-                MyButton.cantClick = true;
-                if (!MyButton.firstPlayer) {
+              MyButton.cantClick = true;
+              if (!MyButton.firstPlayer) {
                     finalLabel.setText("Player 1 wins");
                     layeredPane.add(finalLabel, new Integer(100));
-                }else if (MyButton.firstPlayer) {
+                  }else if (MyButton.firstPlayer) {
                     finalLabel.setText("Player 2 wins");
                     layeredPane.add(finalLabel, new Integer(100));
                   }
-            }if (MyButton.numClicked == 9 && !MyButton.cantClick) {
-                finalLabel.setText("Draw");
-                layeredPane.add(finalLabel, new Integer(100));
+            } if (MyButton.numClicked == 9 && !MyButton.cantClick) {
+              finalLabel.setText("Draw");
+              layeredPane.add(finalLabel, new Integer(100));
             }
         }
     }
@@ -88,19 +88,3 @@ public class Tictactoe implements ActionListener {
 }
 
 
-// while (true) {
-//   System.out.println("");                         // without this line cannot execute if statement
-//   if (MyButton.cantClick && !MyButton.firstPlayer) {
-//     finalLabel.setText("Player 1 wins");
-//     layeredPane.add(finalLabel, new Integer(100));
-//     break;
-//   }else if (MyButton.cantClick && MyButton.firstPlayer) {
-//     finalLabel.setText("Player 2 wins");
-//     layeredPane.add(finalLabel, new Integer(100));
-//     break;
-//   }else if (MyButton.numClicked == 9 && !MyButton.cantClick) {
-//     finalLabel.setText("Draw");
-//     layeredPane.add(finalLabel, new Integer(100));
-//     break;
-//   }
-// }
